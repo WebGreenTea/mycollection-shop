c001f1c91fa8b21930a3c8598d7751f3ffbd3263
Sahawuth Boonyurn <52134599+WebGreenTea@users.noreply.github.com>
2024-09-04 23:45:02 +0700

    test image display

--- app.vue
+++ app.vue
@@ -31,7 +31,8 @@
       <v-row>
         <v-col cols="6" lg="2" md="4" sm="4" xs="6" :class="cardClass">
           <v-card href="https://s.shopee.co.th/3q442ue7Wz">
-            <v-img :src="test"></v-img>
+            <!-- <v-img :src="test"></v-img> -->
+             <img src="https://down-th.img.susercontent.com/file/th-11134207-7r98p-lyx8vfong4h5c5" alt="">
             <v-card-title>WLMouse Beast X 8K</v-card-title>
           </v-card>
         </v-col>
